a42f19bca847e6ac2216a55cce309afc17d55371
vignesh0923 <vigneshanitha0923@gmail.com>
2024-04-04 21:06:39 +0530

    dd

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -22,35 +22,35 @@ const Projects = () => {
     <div className="content">
       <h2 className="title">School Website</h2>
       <p className="copy">The school website project involves both static and dynamic content, facilitating updates and content changes on live websites.</p>
-      <button className="btn">Visit Site</button>
+      <button className="btn"><a href="https://sfsschoolpudukkottai.org/">Visit Site</a></button>
     </div>
   </div>
   <div className="card">
     <div className="content">
       <h2 className="title">Students Management System</h2>
       <p className="copy">In the Students Management System project, my responsibilities include designing and implementing a responsive UI for optimal user experience across devices, ensuring accessibility and functionality enhancements.</p>
-      <button className="btn">Visit Site</button>
+      <button className="btn"><a href="">Visit Site</a></button>
     </div>
   </div>
   <div className="card">
     <div className="content">
       <h2 className="title">Teachers Management System</h2>
       <p className="copy">In the Teachers Management System project, my responsibilities include designing and implementing a responsive UI for optimal user experience across devices, ensuring accessibility and functionality enhancements.</p>
-      <button className="btn">Visit Site</button>
+      <button className="btn"><a href="">Visit Site</a></button>
     </div>
   </div>
   <div className="card">
     <div className="content">
       <h2 className="title">Basketball Academy Website</h2>
       <p className="copy">The Basketball Academy website project involves both static and dynamic content, facilitating updates and content changes on live websites.</p>
-      <button className="btn">Visit Site</button>
+      <button className="btn"><a href="https://vprshoopsacademy.org/">Visit Site</a></button>
     </div>
   </div>
   <div className="card">
     <div className="content">
       <h2 className="title">Students CRUD Application</h2>
       <p className="copy">In the student CRUD application, we manage student details with create, delete, update, and read operations, storing data in an SQL database and retrieving all information seamlessly.</p>
-      <button className="btn">Visit Site</button>
+      <button className="btn"><a href="">Visit Site</a></button>
     </div>
   </div>
 </main>
